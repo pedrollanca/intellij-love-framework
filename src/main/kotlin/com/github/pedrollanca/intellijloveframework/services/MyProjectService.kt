b70ae8206c6e2aa9aa555897688834f9f9e852c8
GitHub Action <action@github.com>
2024-12-24 00:03:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/pedrollanca/intellijloveframework/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pedrollanca/intellijloveframework/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.pedrollanca.intellijloveframework.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pedrollanca.intellijloveframework.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
